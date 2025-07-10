87eb107cd9dc83f0d730b04ba3b46de421f90a2f
xiaban <xiabanhuang@tencent.com>
2025-07-10 15:49:56 +0800

    test c1 no

--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to 123213213 nextjs.org →
+          Not go to 123213213 nextjs.org →
         </a>
       </footer>
     </div>
