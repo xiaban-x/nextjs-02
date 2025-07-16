e9ff605e0808ac71ac0b73d7f36b61d72963d1bc
xiaban <980892894@qq.com>
2025-07-16 15:11:04 +0800

    feat: add 2

--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to my-dev-branch777 nextjs.org →
+          Go to my-dev nextjs.org →
         </a>
       </footer>
     </div>
